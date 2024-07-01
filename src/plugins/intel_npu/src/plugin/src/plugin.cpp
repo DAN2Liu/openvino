3f38fd32ad166d37581464e18b153bdfa51067ff
Liu, Dan1 <dan1.liu@intel.com>
2024-07-01 16:37:49 +0800

    add fix for log not balence

--- src/plugins/intel_npu/src/plugin/src/plugin.cpp
+++ src/plugins/intel_npu/src/plugin/src/plugin.cpp
@@ -155,10 +155,25 @@ void update_log_level(const std::map<std::string, std::string>& propertiesMap) {
 
 namespace intel_npu {
 
+
 static Config merge_configs(const Config& globalConfig,
                             const std::map<std::string, std::string>& rawConfig,
                             OptionMode mode = OptionMode::Both) {
     Config localConfig = globalConfig;
+    auto it = propertiesMap.find(std::string(LOG_LEVEL::key()));
+    
+    if (it != propertiesMap.end()) {
+        std::istringstream is(it->second);
+        ov::log::Level level;
+        is >> level;
+        if (localConfig.get<LOG_LEVEL> > level) {
+            it.remove();
+        } else {
+            Logger::global().setLevel(level);
+            //note: _globalConfig alse need to be update for any maybe call for _globalConfig.
+        }
+    }
+
     localConfig.update(rawConfig, mode);
     if(rawConfig.find(std::string(LOG_LEVEL::key())) != rawConfig.end()) {
         Logger::global().setLevel(localConfig.get<LOG_LEVEL>());
@@ -548,7 +563,8 @@ Plugin::Plugin()
 
 void Plugin::set_property(const ov::AnyMap& properties) {
     const std::map<std::string, std::string> config = any_copy(properties);
-    update_log_level(config);
+    // update_log_level(config); is it better to use global log to update?
+    //Logger::global().setLevel(_globalConfig.get<LOG_LEVEL>());
     for (const auto& configEntry : config) {
         if (_properties.find(configEntry.first) == _properties.end()) {
             OPENVINO_THROW("Unsupported configuration key: ", configEntry.first);
@@ -597,7 +613,7 @@ std::shared_ptr<ov::ICompiledModel> Plugin::compile_model(const std::shared_ptr<
     }
 
     const std::map<std::string, std::string> propertiesMap = any_copy(properties);
-    update_log_level(propertiesMap);
+    // update_log_level(propertiesMap);
     auto localConfig = merge_configs(_globalConfig, propertiesMap);
 
     const auto set_cache_dir = localConfig.get<CACHE_DIR>();
@@ -693,7 +709,7 @@ std::shared_ptr<ov::ICompiledModel> Plugin::import_model(std::istream& stream, c
     OV_ITT_TASK_CHAIN(PLUGIN_IMPORT_MODEL, itt::domains::NPUPlugin, "Plugin::import_model", "merge_configs");
 
     const std::map<std::string, std::string> propertiesMap = any_copy(properties);
-    update_log_level(propertiesMap);
+    // update_log_level(propertiesMap);
     auto localConfig = merge_configs(_globalConfig, propertiesMap, OptionMode::RunTime);
     const auto platform = _backends->getCompilationPlatform(localConfig.get<PLATFORM>(), localConfig.get<DEVICE_ID>());
     localConfig.update({{ov::intel_npu::platform.name(), platform}});
@@ -762,7 +778,7 @@ ov::SupportedOpsMap Plugin::query_model(const std::shared_ptr<const ov::Model>& 
                                         const ov::AnyMap& properties) const {
     OV_ITT_SCOPED_TASK(itt::domains::NPUPlugin, "Plugin::query_model");
     const std::map<std::string, std::string> propertiesMap = any_copy(properties);
-    update_log_level(propertiesMap);
+    // update_log_level(propertiesMap);
     auto localConfig = merge_configs(_globalConfig, propertiesMap, OptionMode::CompileTime);
     const auto platform = _backends->getCompilationPlatform(localConfig.get<PLATFORM>(), localConfig.get<DEVICE_ID>());
     localConfig.update({{ov::intel_npu::platform.name(), platform}});
